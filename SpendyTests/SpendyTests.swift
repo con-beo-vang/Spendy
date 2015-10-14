6931999777fab5f33de5416b97ca890f4065da6e
nguyen.huynh+thien.lam <nguyen.huynh+thien.lam@eastagile.com>
2015-10-14 17:28:57 +0700

    simple test cases for Account

--- SpendyTests/SpendyTests.swift
+++ SpendyTests/SpendyTests.swift
@@ -6,31 +6,81 @@
 //  Copyright (c) 2015 Cheetah. All rights reserved.
 //
 
-import UIKit
-import XCTest
+import Quick
+import Nimble
 
-class SpendyTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+@testable import Spendy
+
+class AccountSpec: QuickSpec {
+  override func spec() {
+
+    var account: Account!
+    var toAccount: Account!
+
+    beforeEach {
+      //      account = Account(name: "Cash")
+      account = Account(name: "Cash", startingBalance: 100.00)
+      account.recomputeBalance()
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+
+    describe("starting balance") {
+      it("new account with starting balance 100.00") {
+        expect(account.balance).to(beCloseTo(100.00, within: 0.0001))
+      }
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+
+    describe("#formattedBalance") {
+      it("formatted balance with region US") {
+        expect(account.formattedBalance()).to(equal("$100.00"))
+      }
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
+
+    describe("#addTransaction") {
+
+      context("expense kind") {
+        beforeEach {
+          let bookCategory = Category.findById("RXhPRosXiF")
+          let transaction = Transaction(kind: Transaction.expenseKind, note: "", amount: 20, category: bookCategory, account: account, date: NSDate())
+          account.addTransaction(transaction)
         }
+
+        it("add new transaction, decrease balance") {
+          expect(account.balance).to(beCloseTo(80.00, within: 0.0001))
+        }
+      }
+
+      context("income kind") {
+        beforeEach {
+          let bonusCategory = Category.findById("sgk8obYqDy")
+          let transaction = Transaction(kind: Transaction.incomeKind, note: "", amount: 50, category: bonusCategory, account: account, date: NSDate())
+          account.addTransaction(transaction)
+        }
+
+        it("add new transaction, increase balance") {
+          expect(account.balance).to(beCloseTo(150.00, within: 0.0001))
+        }
+      }
+
+      fcontext("transfer kind") {
+        beforeEach {
+          // define toAccount
+          toAccount = Account(name: "Salary")
+
+          let transaction = Transaction(kind: Transaction.transferKind, note: "", amount: 30, category: Category.defaultTransferCategory(), account: account, date: NSDate())
+          transaction.toAccount = toAccount
+          account.addTransaction(transaction)
+          toAccount.addTransaction(transaction)
+        }
+
+        it("add new transfer transaction, decrease balance in fromAccount") {
+          expect(account.balance).to(beCloseTo(70.00, within: 0.0001))
+        }
+
+        it("add new transfer transaction, increase balance in toAccount") {
+          expect(toAccount.balance).to(beCloseTo(30.00, within: 0.0001))
+        }
+      }
     }
-    
+  }
 }
+
