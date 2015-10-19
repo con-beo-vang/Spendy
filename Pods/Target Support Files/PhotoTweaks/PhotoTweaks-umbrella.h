d08e091883a7b32b188dfc04ba4dadfdbdd2452f
Harley Trung <harley@bsa.edu.vn>
2015-10-19 14:12:02 +0700

    from now on, run 'pod install' after someone else adds a pod. add '--verbose' to troubleshoot

--- Pods/Target Support Files/PhotoTweaks/PhotoTweaks-umbrella.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "PhotoTweaksViewController.h"
-#import "PhotoTweakView.h"
-#import "UIColor+Tweak.h"
-
-FOUNDATION_EXPORT double PhotoTweaksVersionNumber;
-FOUNDATION_EXPORT const unsigned char PhotoTweaksVersionString[];
-
--- Pods/Target Support Files/VIPhotoView/VIPhotoView-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "VIPhotoView.h"
-
-FOUNDATION_EXPORT double VIPhotoViewVersionNumber;
-FOUNDATION_EXPORT const unsigned char VIPhotoViewVersionString[];
-
